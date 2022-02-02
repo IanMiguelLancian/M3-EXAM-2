47e38221256123b73ca5cdd7190741aec7efeaf4
JohnRaves-Ranara <JohnRaves-Ranara>
2022-02-02 14:51:27 +0800

    changed ranara html file names, redirected index.html to root

--- exam2/ranara/ranara_portfolio.js
+++ exam2/ranara/ranara_portfolio.js
@@ -1,22 +1,22 @@
 
 function m1quiz2(){
-    document.location.href = "m1quiz2/index.html"
+    document.location.href = "m1quiz2/m1quiz2.html"
 }
 function m1exam(){
-    document.location.href = "m1exam/index.html"
+    document.location.href = "m1exam/m1exam.html"
 }
 function m2quiz1(){
-    document.location.href = "m2quiz1/index.html"
+    document.location.href = "m2quiz1/m2quiz1.html"
 }
 function m2quiz2(){
-    document.location.href = "m2quiz2/index.html"
+    document.location.href = "m2quiz2/m2quiz2.html"
 }
 function m2exam(){
-    document.location.href = "m2exam/index.html"
+    document.location.href = "m2exam/m2exam.html"
 }
 function m3quiz2(){
-    document.location.href = "m3quiz2/index.html"
+    document.location.href = "m3quiz2/m3quiz2.html"
 }
 function m3exam(){
-    document.location.href = "m3exam/index.html"
+    document.location.href = "m3exam/m3exam.html"
 }
