a11a44acdaf3acfca7a778607ecc5a683fd3ebf7
IanMiguelLancian <imLancian@mcm.edu.ph>
2022-02-02 11:41:34 +0800

    Create lancian_portfolio.js

---
+++ exam2/lancian/lancian_portfolio.js
@@ -0,0 +1,21 @@
+function mod1exam(){
+    document.location.href = "mod1exam/exam1.html"
+}
+function mod1quiz2(){
+    document.location.href = "mod1quiz2/quiz2.html"
+}
+function mod2quiz1(){
+    document.location.href = "mod2quiz1/quiz1.html"
+}
+function mod2quiz2(){
+    document.location.href = "mod2quiz2/quiz2.html"
+}
+function mod2exam(){
+    document.location.href = "mod2exam/main.html"
+}
+function mod3quiz2(){
+    document.location.href = "mod3quiz2/main.html"
+}
+function mod3exam(){
+    document.location.href = "mod3exam/exam.html"
+}
